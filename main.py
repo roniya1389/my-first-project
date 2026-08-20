8befc013193d839b994ec4600dbfbd3525f02ce0
Ronya <roniyadelkhosh@gmail.com>
2026-08-20 00:00:41 -0700

    add main python file

---
+++ main.py
@@ -0,0 +1,50 @@
+#string(str)
+print("3"+"9")
+print("8"+"67")
+
+#integer(int)
+print(6+9)
+print(78+22)
+
+#float(float)
+print(3.2+5.2)
+print(63.52)
+
+#boolean(bool)
+print(bool(0))
+print(bool(1))
+
+#arithmetic operators
+print(9+11)
+print(5*3)
+print(8**5)
+print(78/8)
+print(56//5)
+print(9%7)
+
+#concatenation
+print("R"+"o"+"n"+"i"+"a")
+
+#whitespace
+print("d"+" "+"y"+" "+"R")
+
+#writing cases
+#snake_case->my_car
+#SCREAMING_SNAKE_CASE->MY_CAR
+#PascalCase->MyCar
+#camelCase->myCar
+#kebab-case->my-car
+#underscore(_)
+#hyphen(-)
+
+#variable
+X=43
+x=542
+print(X)
+X=0.32658
+print(X)
+X="salam"
+X="hello"
+print(X)
+
+#1.Can use alpha.digits and (_) underscore
